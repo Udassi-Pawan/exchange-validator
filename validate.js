0f226b322880229d7d867ffa887f70100204c820
Pawan Udassi <pawanudassi@hotmail.com>
2023-08-01 21:21:21 +0530

    added custom setTimeout

--- validate.js
+++ validate.js
@@ -2,6 +2,8 @@ import { ethers } from "ethers";
 import abi from "./abi.js";
 import { getSignedContract } from "./helper.js";
 import dotenv from "dotenv";
+import { setTimeout } from "timers/promises";
+
 dotenv.config();
 
 const sepoliaContract = getSignedContract("11155111", process.env.pk);
@@ -10,71 +12,73 @@ const mumbaiContract = getSignedContract("80001", process.env.pk);
 console.log("Validator node up and running.");
 console.log("Waiting for Transactions to validate.");
 
+const waitTime = Math.random() * 1000;
+console.log("start");
+const result = await setTimeout(waitTime * 10, () => {});
+console.log("waited");
+
 sepoliaContract.on("nftBurned", async (sender, uri, nonce) => {
   const waitTime = Math.random() * 1000;
   console.log("Tx received. wait ", waitTime * 10);
-  setTimeout(async () => {
-    console.log("##############################################");
-    console.log("attesting ", sender, uri, Number(nonce));
-    console.log("##############################################");
-    const tx = await mumbaiContract.attestTransaction(
-      Number(nonce),
-      uri,
-      sender,
-      0
-    );
-    console.log(await tx.wait());
-  }, 10 * waitTime);
+  const result = await setTimeout(waitTime * 10, "resolved");
+
+  console.log("##############################################");
+  console.log("attesting ", sender, uri, Number(nonce));
+  console.log("##############################################");
+  const tx = await mumbaiContract.attestTransaction(
+    Number(nonce),
+    uri,
+    sender,
+    0
+  );
+  console.log(await tx.wait());
 });
 
 sepoliaContract.on("ethReceived", async (amount, sender, nonce) => {
   const waitTime = Math.random() * 1000;
-  console.log("Tx received. wait ", waitTime * 10);
-
-  setTimeout(async () => {
-    console.log("##############################################");
-    console.log("attesting ", Number(amount), sender, Number(nonce));
-    console.log("##############################################");
-    const tx = await mumbaiContract.attestTransaction(
-      Number(nonce),
-      "",
-      sender,
-      Number(amount)
-    );
-    console.log(await tx.wait());
-  }, 10 * waitTime);
+  const result = await setTimeout(waitTime * 10, "resolved");
+  console.log("##############################################");
+  console.log("attesting ", Number(amount), sender, Number(nonce));
+  console.log("##############################################");
+  const tx = await mumbaiContract.attestTransaction(
+    Number(nonce),
+    "",
+    sender,
+    Number(amount)
+  );
+  console.log(await tx.wait());
 });
 
 mumbaiContract.on("nftBurned", async (sender, uri, nonce) => {
   const waitTime = Math.random() * 1000;
   console.log("Tx received. wait ", waitTime * 10);
-  setTimeout(async () => {
-    console.log("##############################################");
-    console.log("attesting ", sender, uri, Number(nonce));
-    console.log("##############################################");
-    const tx = await sepoliaContract.attestTransaction(
-      Number(nonce),
-      uri,
-      sender,
-      0
-    );
-    console.log(await tx.wait());
-  }, 10 * waitTime);
+  const result = await setTimeout(waitTime * 10, "resolved");
+
+  console.log("##############################################");
+  console.log("attesting ", sender, uri, Number(nonce));
+  console.log("##############################################");
+  const tx = await sepoliaContract.attestTransaction(
+    Number(nonce),
+    uri,
+    sender,
+    0
+  );
+  console.log(await tx.wait());
 });
 
 mumbaiContract.on("ethReceived", async (amount, sender, nonce) => {
   const waitTime = Math.random() * 1000;
   console.log("Tx received. wait ", waitTime * 10);
-  setTimeout(async () => {
-    console.log("##############################################");
-    console.log("attesting ", Number(amount), sender, Number(nonce));
-    console.log("##############################################");
-    const tx = await sepoliaContract.attestTransaction(
-      Number(nonce),
-      "",
-      sender,
-      Number(amount)
-    );
-    console.log(await tx.wait());
-  }, 10 * waitTime);
+  const result = await setTimeout(waitTime * 10, "resolved");
+
+  console.log("##############################################");
+  console.log("attesting ", Number(amount), sender, Number(nonce));
+  console.log("##############################################");
+  const tx = await sepoliaContract.attestTransaction(
+    Number(nonce),
+    "",
+    sender,
+    Number(amount)
+  );
+  console.log(await tx.wait());
 });
