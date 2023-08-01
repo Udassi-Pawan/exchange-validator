5c4f3f77bb1e90e623be683e3480084e2e3a0270
Pawan Udassi <pawanudassi@hotmail.com>
2023-08-01 20:49:42 +0530

    added timeout

--- validate.js
+++ validate.js
@@ -7,58 +7,76 @@ dotenv.config();
 const sepoliaContract = getSignedContract("11155111", process.env.pk);
 const mumbaiContract = getSignedContract("80001", process.env.pk);
 
-sepoliaContract.on("nftBurned", async (sender, uri, nonce) => {
-  console.log("##############################################");
-  console.log("attesting ", sender, uri, Number(nonce));
-  console.log("##############################################");
+console.log("Validator node up and running.");
+console.log("Waiting for Transactions to validate.");
 
-  const tx = await mumbaiContract.attestTransaction(
-    Number(nonce),
-    uri,
-    sender,
-    0
-  );
-  console.log(await tx.wait());
+sepoliaContract.on("nftBurned", async (sender, uri, nonce) => {
+  const waitTime = Math.random() * 400;
+  console.log("Tx received. wait ", waitTime * 10);
+  setTimeout(async () => {
+    console.log("##############################################");
+    console.log("attesting ", sender, uri, Number(nonce));
+    console.log("##############################################");
+    const tx = await mumbaiContract.attestTransaction(
+      Number(nonce),
+      uri,
+      sender,
+      0
+    );
+    console.log(await tx.wait());
+  }, 10 * waitTime);
 });
 
 sepoliaContract.on("ethReceived", async (amount, sender, nonce) => {
-  console.log("##############################################");
-  console.log("attesting ", Number(amount), sender, Number(nonce));
-  console.log("##############################################");
+  const waitTime = Math.random() * 400;
+  console.log("Tx received. wait ", waitTime * 10);
 
-  const tx = await mumbaiContract.attestTransaction(
-    Number(nonce),
-    "",
-    sender,
-    Number(amount)
-  );
-  console.log(await tx.wait());
+  setTimeout(async () => {
+    console.log("##############################################");
+    console.log("attesting ", Number(amount), sender, Number(nonce));
+    console.log("##############################################");
+    const tx = await mumbaiContract.attestTransaction(
+      Number(nonce),
+      "",
+      sender,
+      Number(amount)
+    );
+    console.log(await tx.wait());
+  }, 10 * waitTime);
 });
 
 mumbaiContract.on("nftBurned", async (sender, uri, nonce) => {
-  console.log("##############################################");
-  console.log("attesting ", sender, uri, Number(nonce));
-  console.log("##############################################");
+  console.log("Tx received. wait ", waitTime * 10);
 
-  const tx = await sepoliaContract.attestTransaction(
-    Number(nonce),
-    uri,
-    sender,
-    0
-  );
-  console.log(await tx.wait());
+  const waitTime = Math.random() * 400;
+  setTimeout(async () => {
+    console.log("##############################################");
+    console.log("attesting ", sender, uri, Number(nonce));
+    console.log("##############################################");
+    const tx = await sepoliaContract.attestTransaction(
+      Number(nonce),
+      uri,
+      sender,
+      0
+    );
+    console.log(await tx.wait());
+  }, 10 * waitTime);
 });
 
 mumbaiContract.on("ethReceived", async (amount, sender, nonce) => {
-  console.log("##############################################");
-  console.log("attesting ", Number(amount), sender, Number(nonce));
-  console.log("##############################################");
+  console.log("Tx received. wait ", waitTime * 10);
 
-  const tx = await sepoliaContract.attestTransaction(
-    Number(nonce),
-    "",
-    sender,
-    Number(amount)
-  );
-  console.log(await tx.wait());
+  const waitTime = Math.random() * 400;
+  setTimeout(async () => {
+    console.log("##############################################");
+    console.log("attesting ", Number(amount), sender, Number(nonce));
+    console.log("##############################################");
+    const tx = await sepoliaContract.attestTransaction(
+      Number(nonce),
+      "",
+      sender,
+      Number(amount)
+    );
+    console.log(await tx.wait());
+  }, 10 * waitTime);
 });
